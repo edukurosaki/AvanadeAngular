f8765841fb59056c46a5e075e1bac944696f1a78
Eduardo Francisco <kurosaki.yslan@gmail.com>
2021-01-22 21:42:21 -0300

    Desafio Blobs famintos resolvido

--- problemasBásicos/blobs.js
+++ problemasBásicos/blobs.js
@@ -1,13 +1,30 @@
+/*
+No planeta Alpha vive a criatura Blobs, que come precisamente 1/2 de seu suprimento
+de comida disponível todos os dias. Escreva um algoritmo que leia a capacidade inicial
+de suprimento de comida (em Kg), e calcule quantos dias passarão antes que Blobs coma
+todo esse suprimento até restar um quilo ou menos.
+
+Entrada
+A primeira linha de entrada contem um único inteiro N (1 ≤ N ≤ 1000), indicando o
+número de casos de teste. As N linhas seguintes contém um valor de ponto flutuante
+C (1 ≤ C ≤ 1000) correspondente à quantidade de comida disponível para Blobs.
+
+Saída
+Para cada caso de teste, imprima uma linha contendo o número de dias que Blobs irá
+demorar para comer todo seu suprimento de comida, seguido da palavra "dias".
+*/
 
 
-// complete as variaveis necessarias
-var n = parseInt();
+var nBlobs = parseInt(gets()); //pega a quantidade de blobs
 
-while ( > 0) { //complete o codigo
-  var c = parseFloat();
-  let dias = ; //atribua o valor correto a variavel
-  while (   ) { // complete o while
-    // complete o codigo
+
+while (nBlobs > 0) { 
+  var nFoods = parseFloat(gets()); //pega a quantidade de alimentos em kg
+  let dias = 0; 
+  while (nFoods > 1   ) { 
+    nFoods/=2;
+    dias++;
   }
-  console.log(`  dias`); //complete o código para a saída
+  console.log(`${dias}  dias`);
+  nBlobs--
 }
