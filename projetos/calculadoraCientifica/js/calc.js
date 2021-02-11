e3fe25d693fbd997e30ef062f0b6c20c083058b0
Eduardo Francisco <kurosaki.yslan@gmail.com>
2021-02-11 01:57:11 -0300

    Subindo Projeto de estudo inicial Finanças do lar

--- projetos/calculadoraCientifica/js/calc.js
+++ projetos/calculadoraCientifica/js/calc.js
@@ -13,10 +13,42 @@ if (tipo === 'acao') {
 
     if (valor === "=") {
         var campoValor = document.getElementById('display').value
-       document.getElementById('display').value = eval(campoValor)
+       document.getElementById('display').value = parseFloat(eval(campoValor))
+       
+    }
+    if (valor === "cos") {
+        numberCos = document.getElementById('display').value
+        document.getElementById('display').value = Math.cos(numberCos)
+        
+    }
+    if (valor === "sin") {
+        numberSen = document.getElementById('display').value
+        document.getElementById('display').value = Math.sin(numberSen)
+    }
+    if (valor === "tan") {
+        numberTan = document.getElementById('display').value
+        document.getElementById('display').value = Math.tan(numberTan)
+    }
+    if (valor === "del") {
+       valorIns = document.getElementById('display').value
+       document.getElementById('display').value = valorIns.substring(0, valorIns.length - 1)
+    }
+    if (valor === "e^") {
+       valorExponencial = document.getElementById('display').value
+       document.getElementById('display').value = Math.exp(valorExponencial)
+    }
+    if (valor === "x2") {
+       valorPotencia = document.getElementById('display').value
+       document.getElementById('display').value = Math.pow(valorPotencia, 2)
+    }
+    if (valor === "√") {
+       valorRaiz = document.getElementById('display').value
+       document.getElementById('display').value = Math.pow(valorRaiz, 1/2)
     }
     
 }else if(tipo === 'valor'){
    
     document.getElementById('display').value += valor
-}}
+    
+}
+}
---
+++ projetos/controleFinanceiro/scripts/finans.js
@@ -0,0 +1,8 @@
+var contas = {}
+contas = {
+    description: "descicao",
+    date_in: "dateEntry",
+    date_on: "dataDeparture",
+    expense: "payment"
+
+}
